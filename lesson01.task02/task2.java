cb636e719b0a77c5672ff8ef1b72ddf714502907
Alexandr <36471904+Ostan1n@users.noreply.github.com>
2020-03-03 08:25:23 +0300

    Update task2.java

--- lesson01.task02/task2.java
+++ lesson01.task02/task2.java
@@ -1,29 +1,32 @@
+import java.util.Random; // импорт рандома
+import java.util.Scanner; // импорт сканера
+
+/**
+ * @author Александр Останин.
+ */
+
 public class task2 {
-
-    /**
-     * Main method
-     * @param args
-     */
-
-    public static void main(String[] args) {
-
-    /**
-     * @param array[i] Генерирует случайно число на промежутке [-15,15]
-     * @param q Вычисляет квадратный корень целой части числа из массива array
-     */
-
-    int[] array;
-        array = new int[5];
-        for (int i = 0; i < array.length; i++) {
-        array[i] = (int) Math.round((Math.random() * 30) - 15);
-        double q = (int) (Math.sqrt(array[i]));
-        if (array[i] < 0) {
-            System.out.println("Из этого числа корень не извлечь: " + array[i] + " " + "оно отрицательное");
-        } else if (q * q == array[i]) {
-            System.out.println("Ответ: " + array[i]);
-            System.out.println("Квадратный корень " + q);
+    public static void main(String args[]) {
+        Random random = new Random(); // Инициализируем генератор
+        Scanner scanner = new Scanner(System.in); // Инициализируем сканер
+        int n = scanner.nextInt(); // Считать с консоли
+        for (int i = 0; i < n; i++) {
+            int k = random.nextInt(100); // Получаем случайное число в диапазоне от min до max (включительно)
+            // Обрабатываем исключения через try catch
+            try {
+                if (k < 0) {
+                    throw new IllegalArgumentException();
+                }
+            } catch (IllegalArgumentException e) {
+                continue;
+            }
+            double q = Math.sqrt(k); // кв.корень
+            if (((int) q) * ((int) q) == k) {
+                System.out.println(k); // выводим ответ
+            }
         }
-    }
+
     }
 }
+
 
